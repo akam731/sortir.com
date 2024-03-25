0bab231231fe479a9a3b90c9e15f0fc7b93b1275
Alexandre <alexandre.marteau63@gmail.com>
2024-03-25 14:05:40 +0100

    Début du header

---
+++ src/Controller/MainController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController extends AbstractController
+{
+
+    #[Route('/', name: 'main_home')]
+    public function home()
+    {
+        return $this->render('main/connexion.html.twig');
+    }
+
+    #[Route('/connexion', name: 'main_connexion')]
+    public function index(): Response
+    {
+        return $this->render('main/connexion.html.twig');
+    }
+}
