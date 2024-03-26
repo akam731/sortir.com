e9d26adc5db7d514a6c9134bcb8a70e15bc525c0
Alexandre <alexandre.marteau63@gmail.com>
2024-03-26 15:47:33 +0100

    Correction bug Correction du bug dans le controller main

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -12,7 +12,7 @@ class MainController extends AbstractController
     #[Route('/', name: 'main_home')]
     public function home()
     {
-        return $this->render('main/connexion.html.twig');
+        return $this->render('main/home.html.twig');
     }
 
 }
