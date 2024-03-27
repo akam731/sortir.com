7bca00eb2527d35819f240870cefdd18ced6f216
Alexandre <alexandre.marteau63@gmail.com>
2024-03-27 19:39:17 +0100

    Modifications Modification du header pour qu'il affiche le bouton admin seulement pour les administrateurs

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -6,10 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/admin', name: 'app_admin')]
+#[Route('/admin', name: 'admin_')]
 class AdminController extends AbstractController
 {
-    #[Route('', name: 'admin_home')]
+    #[Route('', name: 'home')]
     public function home(): Response
     {
         return $this->render('admin/index.html.twig', [
