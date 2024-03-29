2713303686c424e7dfaaadef3d90a4d0ec206f57
Alexandre <alexandre.marteau63@gmail.com>
2024-03-30 00:45:14 +0100

    Admin Fin de la gestion des campus, villes, users

--- src/Controller/CityController.php
+++ src/Controller/CityController.php
@@ -50,34 +50,6 @@ class CityController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'app_city_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $city = new City();
-        $form = $this->createForm(CityType::class, $city);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($city);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_city_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('city/new.html.twig', [
-            'city' => $city,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_city_show', methods: ['GET'])]
-    public function show(City $city): Response
-    {
-        return $this->render('city/show.html.twig', [
-            'city' => $city,
-        ]);
-    }
-
     #[Route('/{id}/edit', name: 'app_city_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, City $city, EntityManagerInterface $entityManager): Response
     {
