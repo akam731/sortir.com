e4214fe5d7669627d7631d700f110eadb53bdd45
Papy-La-Taupe <c.combes.pro@gmail.com>
2024-03-30 14:41:17 +0100

    Modif : bricolage event3
    ajout submit button dans EventSearchType

--- src/Form/EventSearchType.php
+++ src/Form/EventSearchType.php
@@ -8,6 +8,7 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -79,6 +80,9 @@ class EventSearchType extends AbstractType
             ->add('pastEvent', CheckboxType::class, [
                 'label' => 'Sorties récemment terminées',
                 'required' => false,
+            ])
+            ->add('search', SubmitType::class, [
+                'label'=> 'Rechercher',
             ])
         ;
     }
