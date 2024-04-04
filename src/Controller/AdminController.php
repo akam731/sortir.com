7a734ce1a7497f76f80ed4db7cc05335281d5a9e
Alexandre <alexandre.marteau63@gmail.com>
2024-04-04 20:22:30 +0200

    Début fonctionalité CSV

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -14,6 +15,29 @@ class AdminController extends AbstractController
     {
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
+        ]);
+    }
+
+    #[Route('/user/csv', name: 'home_addUserByCsv')]
+    public function addUserByCsv(Request $request): Response
+    {
+        $form = $this->createForm(ImportUsersFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Traitement du fichier CSV
+            $file = $form->get('csvFile')->getData();
+            $userManager->importUsersFromCsv($file);
+
+            // Affichage d'un message de succès
+            $this->addFlash('success', 'Users imported successfully.');
+
+            // Redirection vers une autre page ou affichage d'une vue de confirmation
+            return $this->redirectToRoute('admin_dashboard');
+        }
+
+        return $this->render('admin/import_users.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
