e795553568dae0363dfecb70f68a6d791afef79a
Alexandre <alexandre.marteau63@gmail.com>
2024-04-04 20:56:06 +0200

    Création ajout d'user par CSV

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,9 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\ImportUsersFormType;
+use App\Repository\CampusRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
+use League\Csv\Exception;
+use League\Csv\InvalidArgument;
+use League\Csv\Reader;
+use League\Csv\UnavailableStream;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/admin', name: 'admin_')]
@@ -18,25 +28,52 @@ class AdminController extends AbstractController
         ]);
     }
 
+    /**
+     * @throws UnavailableStream
+     * @throws InvalidArgument
+     * @throws Exception
+     */
     #[Route('/user/csv', name: 'home_addUserByCsv')]
-    public function addUserByCsv(Request $request): Response
+    public function addUserByCsv(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, CampusRepository $campusRepository): Response
     {
         $form = $this->createForm(ImportUsersFormType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // Traitement du fichier CSV
+
             $file = $form->get('csvFile')->getData();
-            $userManager->importUsersFromCsv($file);
+            $csvReader = Reader::createFromPath($file, 'r');
+            $csvReader->setDelimiter(';');
+            $records = $csvReader->getRecords();
+            $i = 0;
+            foreach ($records as $record) {
 
-            // Affichage d'un message de succès
-            $this->addFlash('success', 'Users imported successfully.');
+                if($i != 0) {
+                    $user = new User();
+                    $user->setCampus($campusRepository->find($record[0]));
+                    $user->setEmail($record[1]);
+                    $user->setRoles([$record[2]]);
+                    $user->setPassword($userPasswordHasher->hashPassword($user, $record[3]));
+                    $user->setLastName($record[4]);
+                    $user->setFirstName($record[5]);
+                    $user->setPhone($record[6]);
+                    $user->setAdministrator((bool)$record[7]);
+                    $user->setActive((bool)$record[8]);
+                    $user->setImgName($record[9]);
+                    $user->setPseudo($record[10]);
+                    $user->setToken($record[11]);
+                    $entityManager->persist($user);
+                }else {
+                    $i++;
+                }
 
-            // Redirection vers une autre page ou affichage d'une vue de confirmation
-            return $this->redirectToRoute('admin_dashboard');
+            }
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin_user');
         }
 
-        return $this->render('admin/import_users.html.twig', [
+        return $this->render('admin/importUsers.html.twig', [
             'form' => $form->createView(),
         ]);
     }
---
+++ src/Form/ImportUsersFormType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+
+class ImportUsersFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('csvFile', FileType::class, [
+                'label' => 'CSV File',
+                'required' => true,
+                'mapped' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'text/csv',
+                            'text/plain',
+                        ],
+                        'mimeTypesMessage' => 'Merci d\'uploader un fichier CSV valide',
+                    ])
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+        ]);
+    }
+}
